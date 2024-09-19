2c095e8a6916de5496141320dcd8dd49b3ad373b
oulkhorfmohamed@gmail.com <moulkhorf@bar.sio.fr>
2024-09-19 11:11:47 +0200

    ConnexionBDD

--- camping/src/main/java/module-info.java
+++ camping/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.camping {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.camping to javafx.fxml;
     exports com.example.camping;
