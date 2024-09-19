a21be3859ecf18bcce038d5e082d9d8d9a30123f
oulkhorfmohamed@gmail.com <moulkhorf@bar.sio.fr>
2024-09-19 16:50:43 +0200

    gestion des aythentifications(connecte à la base)

--- camping/src/main/java/com/example/camping/HelloApplication.java
+++ camping/src/main/java/com/example/camping/HelloApplication.java
@@ -12,8 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("connexion.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Gestion camping!!");
+        Scene scene = new Scene(fxmlLoader.load(), 820, 505);
+        stage.setTitle("Gestion camping!");
         stage.setScene(scene);
         stage.show();
     }
