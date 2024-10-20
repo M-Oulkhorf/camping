8e28cbb2f88356d8c8861e570138bf4714dd0674
SDR SETTAT <yassifradwa@gmail.com>
2024-10-20 15:37:59 +0200

    Style connexion

--- camping/src/main/java/com/example/camping/HelloApplication.java
+++ camping/src/main/java/com/example/camping/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.camping;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("connexion.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 820, 505);
-        stage.setTitle("Gestion camping!");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/example/camping/logo1.png"))); // Utilisez le chemin relatif
+        stage.setTitle("PlaniCamp");
         stage.setScene(scene);
         stage.show();
     }
